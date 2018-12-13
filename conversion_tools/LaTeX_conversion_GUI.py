11874379d33be54a5042a39c3a5d067935123cb1
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2018-12-13 11:57:17 -0800

    working on copy images dir function and convert GUI

--- conversion_tools/LaTeX_conversion_GUI.py
+++ conversion_tools/LaTeX_conversion_GUI.py
@@ -12,17 +12,17 @@ from pathlib import Path
 
 from gooey import Gooey, GooeyParser
 
-from converter_functions import (
+from convert_functions import copy_all_images_to_dir
+from convert_functions import (
     file_to_nbnode,
     export_nbnode,
 
 )
 
 
-@Gooey(dump_build_config=True, program_name="Jupter Notebook Conversion Tool (convert directory of notebooks)")
+@Gooey(dump_build_config=True, program_name="Jupter Notebook Conversion Tool")
 def main():
     desc = "A Python GUI App to convert a directory of Jupyter Notebooks into one big LaTeX document"
-    template_select_help_msg = "Select a template (.tplx-file) to apply"
 
     my_parser = GooeyParser(description=desc)
 
@@ -30,7 +30,7 @@ def main():
         "Input_Directory", help="Select a directory to filled with notebooks", widget="DirChooser"
     )
     my_parser.add_argument(
-        "Template_File", help=template_select_help_msg, widget="FileChooser"
+        "Template_File", help="Select a template (.tplx-file) to apply", widget="FileChooser"
     )
 
     my_parser.add_argument(
@@ -38,51 +38,52 @@ def main():
     )
 
     my_parser.add_argument(
-        "Output_FileName", help="File Name for Output file", widget="DirChooser"
+        "Output_FileName", help="Output file name (no .tex extension)",
     )
 
+    ## parse the arguments
     args = my_parser.parse_args()
-    # get all of the .ipynb file paths
-    nb_filepath_list = [file for file in (Path(args.Input_Directory)).glob("**/*.ipynb")]
+
+    ## Construct all the file paths
+
+    # construct input directory file path
+    input_dir_Path = Path(args.Input_Directory)
+    print(f'Input directory: {input_dir_Path}')
 
     # construct template file path
     template_file_Path = Path(args.Template_File)
+    print(f'Template file path: {template_file_Path}')
 
-    for f in nb_filepath_list:
-        print('Converting Notebook:')
-        print('infile Path')
-        print(f)
-        nbnode = file_to_nbnode(f)
-        print('Out file Path')
-        outfile_Path = Path(f.parent, Path(f.stem))
-        print(outfile_Path)
+    # construct output directory path
+    output_dir_Path = Path(args.Output_Directory)
+    print(f'Output directory: {output_dir_Path}')
 
-        # create lab_title.tplx file where the lab title from the input notebook file name is derived
-        lab_title_str = extract_lab_title(f)
-        create_lab_title_template(lab_title_str, "lab_title.tplx")
-        export_nbnode(nbnode, outfile_Path, pdf=False, template_file=template_file_Path)
+    outfile_name = args.Output_FileName
+    print(f'Output file name: {outfile_name}')
 
+    print(f'Copying images from: {input_dir_Path} \n into: {output_dir_Path}')
+    copy_all_images_to_dir(input_dir_Path, output_dir_Path, 'images')
 
+    # get all of the .ipynb file paths
+    nb_filepath_list = [file for file in (Path(args.Input_Directory)).glob("**/*.ipynb")]
 
-    # nbnode = file_to_nbnode(args.Notebook_to_Convert)
-    # # construct output .tex file file path
-    # outfile_Path = Path(args.Output_Directory, Path(args.Notebook_to_Convert).stem)
+    #construct template file path
+    template_file_Path = Path(args.Template_File)
     #
-    # # construct template file path
-    # template_file_Path = Path(args.Template_File)
+    # for f in nb_filepath_list:
+    #     print('Converting Notebook:')
+    #     print('infile Path')
+    #     print(f)
+    #     nbnode = file_to_nbnode(f)
+    #     print('Out file Path')
+    #     outfile_Path = Path(f.parent, Path(f.stem))
+    #     print(outfile_Path)
     #
-    # # create lab_title.tplx file where the lab title from the input notebook file name is derived
-    # lab_title_str = extract_lab_title(args.Notebook_to_Convert)
-    # create_lab_title_template(lab_title_str, "lab_title.tplx")
-    #
-    # # export notebook node object to .tex file
-    # export_nbnode(nbnode, outfile_Path, pdf=False, template_file=template_file_Path)
-    #
-    # print(f"input file \n {args.Notebook_to_Convert}")
-    # print()
-    # print(f"output directory \n {args.Output_Directory}")
-    # print()
-    # print(f"template file \n {args.Template_File}")
+    #     # create lab_title.tplx file where the lab title from the input notebook file name is derived
+    #     lab_title_str = extract_lab_title(f)
+    #     create_lab_title_template(lab_title_str, "lab_title.tplx")
+    #     export_nbnode(nbnode, outfile_Path, pdf=False, template_file=template_file_Path)
+
 
 
 if __name__ == "__main__":
--- conversion_tools/convert_functions.py
+++ conversion_tools/convert_functions.py
@@ -4,5 +4,69 @@
 A set of functions to combine a directory of notebooks into a one big notebook,
 then export the one big notebook to a .tex file
 """
+import os
+from pathlib import Path
+import shutil
+import re
+
+from pandocfilters import toJSONFilter, applyJSONFilters, RawInline
+
+def convert_link(key, val, fmt, meta):
+    if key == 'Link':
+        target = val[2][0]
+        # Links to other notebooks
+        m = re.match(r'(\d+\-.+)\.ipynb$', target)
+        if m:
+            return RawInline('tex', 'Chapter \\ref{sec:%s}' % m.group(1))
+
+        # Links to sections of this or other notebooks
+        m = re.match(r'(\d+\-.+\.ipynb)?#(.+)$', target)
+        if m:
+            # pandoc automatically makes labels for headings.
+            label = m.group(2).lower()
+            label = re.sub(r'[^\w-]+', '', label)  # Strip HTML entities
+            return RawInline('tex', 'Section \\ref{%s}' % label)
+
+    # Other elements will be returned unchanged.
 
 
+def convert_links(source):
+    return applyJSONFilters([convert_link], source)
+
+def copy_all_images_to_dir(input_dir_Path, output_dir_Path, output_images_dir_name='images'):
+    """
+    a function to copy the all of the images out of notebooks/subdir/images into
+    one big folder in pdf/images
+    """
+    REG_nb_dir = re.compile((r'(\d\d)-*'))
+
+    images_dir_Path = Path(input_dir_Path,output_dir_Path, output_images_dir_name)
+    # erase the output images dir if it exists
+    if os.path.exists(images_dir_Path):
+        shutil.rmtree(images_dir_Path)
+    # make the output images dir
+    os.makedirs(Path(output_dir_Path, output_images_dir_name))
+
+    for dir in os.listdir(input_dir_Path):
+        if REG_nb_dir.match(dir):
+            if os.path.exists(os.path.join(input_dir_Path, dir, output_images_dir_name)):
+                scr_dir = Path(input_dir_Path, dir, 'images')
+                dst_dir = Path(output_dir_Path, images_dir_Path)
+                for f in os.listdir(scr_dir):
+                    try:
+                        shutil.copy(os.path.join(scr_dir, f), images_dir_Path)
+                    except IOError as e:
+                        print("Unable to copy file. %s" % e)
+                        exit(1)
+                    except:
+                        print("Unexpected error:", sys.exc_info())
+                        exit(1)
+
+
+
+
+def file_to_nbnode():
+    pass
+
+def export_nbnode():
+    pass
